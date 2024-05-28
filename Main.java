b310b958b0c52e887b77dcc99dd8f2f10f0abab9
mrnick368 <mrnick368@gmail.com>
2024-05-28 12:21:09 +0530

    branch 4

--- Main.java
+++ Main.java
@@ -2,7 +2,13 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+<<<<<<< HEAD
 		System.out.println("In Branch B2,Good Afternoon");
 System.out.println("Branch 3 Trial");
+System.out.println("In Branch 4 Good AfterNoon");
+=======
+		System.out.println("In Branch 2 Q2 Good AfterNoon");
+		
+>>>>>>> que2-branch-2
 	}
 }
